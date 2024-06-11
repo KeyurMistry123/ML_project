7fb981ad7c190808ca941a3c36bf7d56d6ff3ff5
unknown <keyur04mistry@gmail.com>
2024-06-11 13:34:20 +0530

    Eight commit

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import find_packages, setup
 from typing import List
 
 hyphen_e_dot = "-e ."
-def get_requirements(file_paths: str)->List[str]:
+def get_requirements(file_paths: str) -> List[str]:
     requirements = []
-    with open("file_paths") as file_obj:
-        requirements = file_obj.readline()
-        requirements = [req.replace("\n","") for req in requirements]
-        
+    with open(file_paths) as file_obj:  # Corrected from "file_paths" to file_paths
+        requirements = file_obj.readlines()  # Changed from readline to readlines to read all lines
+        requirements = [req.strip() for req in requirements]  # Using strip() to remove whitespace and newlines
+
         if hyphen_e_dot in requirements:
             requirements.remove(hyphen_e_dot)
-            
+
     return requirements
         
     
@@ -19,6 +19,6 @@ setup(
     version = "0.0.1",
     author = "Keyur",
     packages = find_packages(),
-    requirements = get_requirements("requirements.txt")
+    requirements = get_requirements(r"C:\Users\Keyur Mistry\Desktop\ML_project\requirements.txt")
 )
  
